149c4d534db2003c701485237ca3827cde97482e
Michael <mnath10@stuy.edu>
2020-06-15 01:28:41 -0400

    created client

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useSpring, animated } from 'react-spring';
+import './App.modules.css';
+
+function App() {
+	const props = useSpring({ opacity: 1, from: { opacity: 0 } });
+	return (
+		<div>
+			<animated.div style={props}>
+				<span className={'node'}>Node</span>fall
+        <h3>Welcome!</h3>
+			</animated.div>
+
+		</div>
+	);
+}
+
+export default App;
---
+++ database/migrations/20200614210004-create-games.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('games', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      location: {
+        type: Sequelize.STRING
+      },
+      difficulty: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('games');
+  }
+};
