a9e98aec0380830b8106210a768a766df1cad30e
Victor Siu <33765960+vs2961@users.noreply.github.com>
2020-06-16 12:24:40 -0400

    Client (#9)
    * homepage
    
    * fix title
    
    * Fix image size
    
    * Fix warnings
    
    Co-authored-by: vs2961 <siuleunging@gmail.com>

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 
+const buttonStyle = {
+    variant: "outlined",
+    size: "large",
+    margin: "auto"
+}
+
 function Home() {
-	return (
-		<div>
-			<span className={'node'}>Node</span>fall
-			<h3>Welcome!</h3>
-			<Button> Create </Button>
-		</div>
-	);
+    return (
+        <div>
+            <img src="Nodefall.gif" width="300px" margin="auto" alt="Nodefall"></img>
+            <br/>
+            <Button {...buttonStyle}>Create a game</Button>
+            <br/>
+            <Button {...buttonStyle}>Join a game</Button>
+        </div>
+    )
 }
 
 export default Home;
