d18199a844f25111f430e0a37952bb86181bc217
Abir Taheer <abir@taheer.me>
2020-06-16 11:34:23 -0400

    renamed restyled config file

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import './App.modules.css';
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Home from "./components/home";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './components/home';
 
 function App() {
 	return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" component={Home} />
-            </Switch>
-        </BrowserRouter>
-            
+		<BrowserRouter>
+			<Switch>
+				<Route path="/" component={Home} />
+			</Switch>
+		</BrowserRouter>
 	);
 }
 
--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 
-
 function Home() {
-    return (
-        <div>
-            <span className={'node'}>Node</span>fall
-            <h3>Welcome!</h3>
-            <Button> Create </Button>
-        </div>
-    )
+	return (
+		<div>
+			<span className={'node'}>Node</span>fall
+			<h3>Welcome!</h3>
+			<Button> Create </Button>
+		</div>
+	);
 }
 
 export default Home;
