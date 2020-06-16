73303be96d6e142bad02f8dde416e3eea836a156
vs2961 <siuleunging@gmail.com>
2020-06-16 09:30:39 -0400

    remove unused imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
 import './App.modules.css';
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./components/home";
 
 function App() {
