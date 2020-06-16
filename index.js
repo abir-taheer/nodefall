ffcd0e8841d303ee2ae53f49d0b117e748b9ddbb
Abir Taheer <abir@taheer.me>
2020-06-16 10:53:04 -0400

    deleted old migrations

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cluster = require('cluster');
 const port = Number(process.env.PORT) || 3001;
 const express = require('express');
 const app = require('./app');
-const path = require("path");
+const path = require('path');
 
 if (process.env.NODE_ENV === 'production') {
 	if (cluster.isMaster) {
@@ -24,9 +24,10 @@ if (process.env.NODE_ENV === 'production') {
 			cluster.fork();
 		});
 	} else {
-
 		app.use(express.static('./client/build'));
-		app.get('*', (req, res) => res.sendFile(path.resolve('./client/build/index.html')));
+		app.get('*', (req, res) =>
+			res.sendFile(path.resolve('./client/build/index.html'))
+		);
 
 		// Code to run inside of a worker
 		app.listen(port, () =>
