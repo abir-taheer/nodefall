9dbadd4151d422cd593b90166a14f2d9f10bdb71
Abir Taheer <40152590+abir-taheer@users.noreply.github.com>
2020-06-16 16:04:58 -0400

    added test for session and error handler(#10)
    * added error handler and tests for session
    
    * Restyled by prettier
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- test/test-server.js
+++ test/test-server.js
@@ -1,15 +1,12 @@
 const app = require('./../app');
 
-describe('App', function () {
-	describe('#listen', function () {
-		it('should run a server without error', done => {
-			let server;
-			try {
-				server = app.listen(process.env.PORT || 3001);
-				done();
-			} finally {
-				server.close();
-			}
-		});
+describe('app', () => {
+	it('server runs without error', () => {
+		let server;
+		try {
+			server = app.listen(process.env.PORT || 3001);
+		} finally {
+			server.close();
+		}
 	});
 });
