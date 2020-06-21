7635bd35934cc4eab9d7b3274edcea414b1eddfa
Abir Taheer <abir@taheer.me>
2020-06-21 05:49:13 -0400

    added state api for getting user status

--- database/models/rooms.js
+++ database/models/rooms.js
@@ -14,5 +14,36 @@ module.exports = (sequelize, DataTypes) => {
 		Rooms.hasMany(models.Players);
 		Rooms.hasMany(models.Games);
 	};
+
+	Rooms.prototype.getIsActive = async () => {
+		if (!this.isActive) {
+			return false;
+		}
+
+		const games = await this.getGames({ sort: ['createdAt', 'DESC'] });
+
+		const maxAge = 1000 * 60 * 60 * 6;
+		const now = new Date();
+		let roomExpiration;
+
+		// Expiration is the last time a change was made to the room
+		if (games.length > 0) {
+			const mostRecentGame = games[0];
+			roomExpiration = new Date(
+				mostRecentGame.updatedAt.getTime() + maxAge
+			);
+		} else {
+			roomExpiration = new Date(this.updatedAt.getTime() + maxAge);
+		}
+
+		if (now >= roomExpiration) {
+			this.update({ isActive: false });
+			return false;
+		}
+
+		// If the room is past the expiration time: return false
+		return true;
+	};
+
 	return Rooms;
 };
--- routes/api/state.js
+++ routes/api/state.js
@@ -1,20 +1,46 @@
 const router = require('express').Router();
-const { Rooms, Players } = require('./../../database');
+const { Players } = require('./../../database');
 
 router.get('/', async (req, res) => {
+	let player, room;
 	let inRoom = false;
 	const playerID = req.session.playerID;
 
 	// If the playerID is set, check to make sure that the room is still occupied
 	if (typeof playerID === 'number') {
-		const player = await Players.findOne({ where: { id: playerID } });
-		const room = await player.getRoom();
-		if (room.isActive) {
-			const games = room.getGames({ sort: ['createdAt', 'DESC'] });
-		} else {
+		player = await Players.findOne({ where: { id: playerID } });
+		room = await player.getRoom();
+
+		const isActive = await room.getIsActive();
+
+		if (!isActive) {
 			inRoom = false;
 			delete req.session.playerID;
 		}
+	}
+
+	if (inRoom) {
+		res.json({
+			success: true,
+			payload: {
+				inRoom: true,
+				player: {
+					name: player.name,
+					room: {
+						publicID: room.publicID,
+						name: room.name,
+						isOwner: player.isOwner
+					}
+				}
+			}
+		});
+	} else {
+		res.json({
+			success: true,
+			payload: {
+				inRoom: false
+			}
+		});
 	}
 });
 
