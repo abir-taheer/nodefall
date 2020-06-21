6c9f7b1e63aa186c570745555e69eb31b33ff0e2
Abir Taheer <abir@taheer.me>
2020-06-20 20:35:49 -0400

    added state api

--- routes/api/state.js
+++ routes/api/state.js
@@ -1,7 +1,35 @@
 const router = require('express').Router();
+const { Rooms, Players } = require('./../../database');
 
-router.get('/', (req, res) => {
-	res.json({});
+router.get('/', async (req, res) => {
+	const playerID = req.session.playerID;
+
+	if (typeof playerID !== 'number') {
+		res.json({
+			success: true,
+			payload: {
+				inRoom: false
+			}
+		});
+	} else {
+		const player = await Players.findOne({ where: { id: playerID } });
+		const room = await player.getRoom();
+
+		res.json({
+			success: true,
+			payload: {
+				inRoom: true,
+				player: {
+					name: player.name,
+					isOwner: player.isOwner,
+					room: {
+						name: room.name,
+						publicID: room.publicID
+					}
+				}
+			}
+		});
+	}
 });
 
 module.exports = router;
