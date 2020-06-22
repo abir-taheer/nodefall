02b8be7eac0e017edb128da47c5ac0ce0aad52c1
Michael <mnath10@stuy.edu>
2020-06-21 21:02:43 -0400

    created lobby page

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import './App.modules.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/home';
 import Create from './components/create';
+import Lobby from './components/lobby';
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Switch>
                 <Route path="/create" component={Create} />
+				<Route path="/lobby" component={Lobby}/>
 				<Route path="/" component={Home} />
 			</Switch>
 		</BrowserRouter>
