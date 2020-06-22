b98a56f5a72cf7510230bd15352c573c27af0217
Abir Taheer <abir@taheer.me>
2020-06-21 23:20:24 -0400

    added API for room lookup

--- routes/api/rooms/index.js
+++ routes/api/rooms/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
 router.use('/create', require('./create'));
+router.use('/:publicID', require('./lookup'));
 
 module.exports = router;
---
+++ routes/api/rooms/lookup.js
@@ -0,0 +1,30 @@
+const router = require('express').Router({ mergeParams: true });
+const { Rooms } = require('./../../../database');
+router.get('/', async (req, res) => {
+	const { publicID } = req.params;
+
+	const room = await Rooms.findOne({ where: { publicID } });
+
+	if (room) {
+		const hasPassword = room.password !== null;
+
+		res.json({
+			success: true,
+			payload: {
+				publicID: room.publicID,
+				name: room.name,
+				hasPassword
+			}
+		});
+	} else {
+		res.status(404).json({
+			success: false,
+			error: {
+				message: 'There is no room with that ID',
+				code: 'ROOM_NOT_FOUND'
+			}
+		});
+	}
+});
+
+module.exports = router;
